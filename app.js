877b63af1515818a0bd796f02a64d867849380ad
Jake Attard <jakeattard18@gmail.com>
2020-11-19 23:20:55 +1000

    Created new folder called views with layouts folder with the main-layout.pug file. Also added pug.

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ const app = express();
 const port = 3001;
 
 const server = app.listen(port, () => console.log("Server listening on port " + port));
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+app.get("/", (req, res, next) => {
+
+    var payload = {
+        pageTitle: "Home"
+    }
+
+    res.status(200).render("home", payload);
+});
