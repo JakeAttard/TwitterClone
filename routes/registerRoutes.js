2815171a81ad9bdc89e3a991fd45d8be2e9bbac9
Jake Attard <jakeattard18@gmail.com>
2020-11-28 15:22:51 +1000

    Added npm package bcrypt to hash password when saving into mongodb.

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser");
+const bcrypt = require('bcrypt');
 const User = require('../schemas/UserSchema');
 
 app.set("view engine", "pug");
@@ -38,6 +39,8 @@ router.post("/", async (req, res, next) => {
         if(user == null) {
             // No User Found
             var data = req.body;
+
+            data.password = await bcrypt.hash(password, 10);
 
             User.create(data)
             .then((user) => {
